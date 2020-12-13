25601a733cdc6dde073576f75563ef0097da5855
Sealra <sebastian.alvarador@usm.cl>
2020-12-12 21:37:10 -0300

    added musixmatch y azure API's

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = function Navbar() {
+    return (
+        <nav className ='navbar navbar-dark bg-dark mg-5'>
+            <span className ='navbar-brand mb-0 h1 mx-auto'>Lyricsfy</span>
+        </nav>
+    )
+};
+
+export default Navbar;
